8debc7e20e1ab718005dcd58f8ae6b12ceeb48c0
metroholographics <nirvaanraja@gmail.com>
2025-01-19 22:02:14 +0000

    water tile fill level, removed 'empty' cell from model, working on update_sim_state but broken at this stage

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 #define WIDTH 900
 #define HEIGHT 600
-#define CELL_SIZE 20
+#define CELL_SIZE 50
 #define COLS (WIDTH / CELL_SIZE)
 #define ROWS (HEIGHT / CELL_SIZE)
 #define COLOR_GRAY 50, 50, 50, 255
@@ -12,14 +12,14 @@
 #define COLOR_BLUE 19, 86, 105, 255
 
 typedef enum cell_types {
-	EMPTY=0,
-	WALL,
+	WALL=0,
 	WATER
 } cell_type;
 
 typedef struct Cell {
 	cell_type type;
 	SDL_FRect cell;
+	double fill_level; 	/* between 0(empty) - 1(full) */
 } cell;
 
 void init_cell_grid(cell cells[][COLS])
@@ -32,7 +32,8 @@ void init_cell_grid(cell cells[][COLS])
 				.w = CELL_SIZE,
 				.h = CELL_SIZE
 			};
-			cells[i][j].type = EMPTY;
+			cells[i][j].type = WATER;
+			cells[i][j].fill_level = 0.0f;
 		}
 	}
 }
@@ -41,9 +42,64 @@ void reset_cells(cell cells[][COLS])
 {
 	for (int i = 0; i < ROWS; i++) {
 		for (int j =0; j < COLS; j++) {
-			cells[i][j].type = EMPTY;
+			cells[i][j].type = WATER;
+			cells[i][j].fill_level = 0.0f;
 		}
 	}
+}
+
+void update_sim_state(cell c[][COLS])
+{
+	double flow_matrix[ROWS][COLS];
+
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			flow_matrix[i][j] = c[i][j].fill_level;
+		}
+	}
+
+	/* :: todo: fix this */
+
+	for (int y = 0; y < ROWS; y++) {
+		for (int x = 0; x < COLS; x++) {
+			double focus_fill = flow_matrix[y][x];
+			if (y < ROWS - 1 && focus_fill > 0.0f) {
+				double below_fill = flow_matrix[y+1][x];
+				if (c[y+1][x].type != WALL) {
+					double total_fill = focus_fill + below_fill;
+					double overflow = 1.0f - total_fill;
+					if (overflow < 0.0f) {
+						flow_matrix[y][x] = overflow;
+						flow_matrix[y+1][x] = 1.0f; 
+					} else {
+						flow_matrix[y][x] = 0.0f;
+						flow_matrix[y+1][x] = total_fill;
+					}
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			c[i][j].fill_level = flow_matrix[i][j];
+		}
+	}
+}
+
+SDL_FRect get_water_tile(cell c) {
+	/*Return properties for a water tile based on position and fill level of passed cell*/
+	double fill = c.fill_level;
+	double empty = 1.0f - fill;
+
+	SDL_FRect r = (SDL_FRect) {
+		.x = c.cell.x,
+		.y = c.cell.y + (empty * c.cell.h),
+		.w = c.cell.w,
+		.h = fill * c.cell.h 
+	};
+
+	return r;
 }
 
 void draw_cells(SDL_Renderer* renderer, cell grid[][COLS])
@@ -52,17 +108,17 @@ void draw_cells(SDL_Renderer* renderer, cell grid[][COLS])
 		for (int j = 0; j < COLS; j++) {
 			cell c = grid[i][j];
 			switch (c.type) {
-				case EMPTY: 
-					SDL_SetRenderDrawColor(renderer, COLOR_GRAY);
-					SDL_RenderRect(renderer, &c.cell);
-					break;
 				case WALL:
 					SDL_SetRenderDrawColor(renderer, COLOR_WHITE);
 					SDL_RenderFillRect(renderer, &c.cell);
 					break;
-				case WATER:
+				case WATER: {
+					SDL_SetRenderDrawColor(renderer, COLOR_GRAY);
+					SDL_RenderRect(renderer, &c.cell);
+					SDL_FRect water_tile = get_water_tile(c);
 					SDL_SetRenderDrawColor(renderer, COLOR_BLUE);
-					SDL_RenderFillRect(renderer, &c.cell);
+					SDL_RenderFillRect(renderer, &water_tile);
+				}
 					break;
 				default:
 					SDL_SetRenderDrawColor(renderer, 230,10,10,255);
@@ -72,6 +128,7 @@ void draw_cells(SDL_Renderer* renderer, cell grid[][COLS])
 		}
 	}
 }
+
 
 bool mouse_in_bounds(float x, float y)
 {
@@ -89,6 +146,7 @@ int main(int argc, char* argv[])
 	SDL_Renderer* renderer;
 	cell cells[ROWS][COLS];
 	cell_type selected_cell = WALL;
+	float default_fill = 0.3f;
 
 	init_cell_grid(cells);
 
@@ -121,8 +179,18 @@ int main(int argc, char* argv[])
 						int c_y = e.motion.y / CELL_SIZE;
 						if (e.motion.state == SDL_BUTTON_LMASK) {
 							cells[c_y][c_x].type = selected_cell;
+
+							if (selected_cell == WATER) {
+								cells[c_y][c_x].fill_level += default_fill;
+								if (cells[c_y][c_x].fill_level > 1.0f) {
+									cells[c_y][c_x].fill_level = 1.0f;
+								}
+							} else {
+								cells[c_y][c_x].fill_level = 0.0f;
+							}
 						} else if (e.motion.state == SDL_BUTTON_RMASK) {
-							cells[c_y][c_x].type = EMPTY;
+							cells[c_y][c_x].type = WATER;
+							cells[c_y][c_x].fill_level = 0.0f;
 						}
 					}
 					break;
@@ -132,8 +200,18 @@ int main(int argc, char* argv[])
 						int c_y = e.button.y / CELL_SIZE;
 						if (e.button.down && e.button.button == SDL_BUTTON_LEFT) {
 							cells[c_y][c_x].type = selected_cell;
+
+							if (selected_cell == WATER) {
+								cells[c_y][c_x].fill_level += default_fill;
+								if (cells[c_y][c_x].fill_level > 1.0f) {
+									cells[c_y][c_x].fill_level = 1.0f;
+								}
+							} else {
+								cells[c_y][c_x].fill_level = 0.0f;
+							}
 						} else if (e.button.down && e.button.button == SDL_BUTTON_RIGHT) {
-							cells[c_y][c_x].type = EMPTY;
+							cells[c_y][c_x].type = WATER;
+							cells[c_y][c_x].fill_level = 0.0f;
 						}
 					}
 					break;
@@ -149,6 +227,8 @@ int main(int argc, char* argv[])
 					break;
 			}
 		}
+
+		update_sim_state(cells);
 
 		draw_cells(renderer, cells);
 
